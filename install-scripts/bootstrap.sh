512a7b0d858a58d94ef0b6390fb5d16086f65f43
Krithic Kumar <krithickumarub@protonmail.com>
2023-03-02 12:03:40 +0530

    update install pkgs and add shell function

--- install-scripts/bootstrap.sh
+++ install-scripts/bootstrap.sh
@@ -2,6 +2,7 @@
 
 # time
 ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
+sudo timedatectl set-ntp true
 hwclock --systohc
 
 # locale
@@ -26,25 +27,33 @@ echo root:password | chpasswd
 pacman -S --no-confim grub grub-btrfs btrfs-progs efibootmgr networkmanager zsh \
 	base-devel linux-headers xdg-user-dirs xdg-utils alsa-utils pipewire \
 	pipewire-alsa pipewire-pulse pipewire-jack openssh rsync reflector os-prober \
-	ntfs-3g pamixer sudo
+	ntfs-3g pamixer sudo opendoas
 
 # wifi and bluetooth stuff
 # pacman -S network-manager-applet dialog wpa_supplicant mtools dosfstools \
 # dnsutils bluez bluez-utils
 
+# update mirrorlist with fastest servers
+reflector -c India -a 12 --sort rate --save /etc/pacman.d/mirrorlist
+
 # user programs
-pacman -S --no-confirm neovim git alacritty unzip bat clipmenu opendoas ranger \
-	lf rustup sccache scrcpy sxiv tealdeer thunar nautilus tmux transmission-cli \
-	ueberzug zathura go python jq firefox dunst ffmpegthumbnailer flameshot \
-	android-tools chromium bpython bat-extras bpytop btop xdg-utils xdg-user-dirs \
-	man-pages man-db gtk3 gtk4 fd zsh-syntax-highlighting dust bc
+pacman -S --no-confirm neovim git alacritty unzip bat ranger lf rustup sccache \
+	scrcpy sxiv tealdeer thunar nautilus tmux transmission-cli ueberzug zathura go \
+	python jq firefox dunst ffmpegthumbnailer flameshot android-tools chromium \
+	bpython bat-extras bpytop btop xdg-utils xdg-user-dirs man-pages man-db gtk3 \
+	gtk4 fd zsh-syntax-highlighting dust bc lxappearance noto-fonts-emoji \
+	obs-studio mpv youtube-dl yt-dlp aria2 zathura-pdf-mupdf stylua strace ripgrep \
+	python-pip pyenv linux-tools mlocate lua jupyter-notebook jre11-openjdk \
+	jre11-openjdk-headless jdk11-openjdk hugo hexyl helix gopls glow flake8 exa \
+	duf ipython arch-install-scripts # jre8-openjdk jre8-openjdk-headless jdk8-openjdk
 
 # xorg
-pacman -S --no-confirm xorg-server xwallpaper xorg-xinit xorg-server xcape \
-	x11-ssh-askpass redshift maim python-pywal rofi xorg-xclipboard xclip
+pacman -S --no-confirm xorg xwallpaper xorg-xinit xcape x11-ssh-askpass \
+	redshift maim python-pywal rofi xorg-xclipboard xclip dmenu rofi-emoji \
+	clipmenu
 
 # GPU drivers
-pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
+pacman -S --noconfirm nvidia nvidia-utils nvidia-settings nvtop cudnn
 # pacman -S --noconfirm xf86-video-amdgpu
 
 # wayland
